40601eb625eca63d896844e179efec4c8b1662a7
alex <alexmajumder55@gmail.com>
2024-10-08 03:25:59 +0600

    demo implement

---
+++ lib/ui/demo Ui.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DemoUI extends StatefulWidget {
+  const DemoUI({super.key});
+
+  @override
+  State<DemoUI> createState() => _DemoUIState();
+}
+
+class _DemoUIState extends State<DemoUI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
